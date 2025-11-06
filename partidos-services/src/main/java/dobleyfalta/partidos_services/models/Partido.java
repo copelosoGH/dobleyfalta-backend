4001b80ff8c4e66acb0bee1a4e95400862a5f09e
Conrado Peloso <conradopeloso.cp@gmail.com>
2025-11-06 13:51:08 -0300

    add apigateway

--- partidos-services/src/main/java/dobleyfalta/partidos_services/models/Partido.java
+++ partidos-services/src/main/java/dobleyfalta/partidos_services/models/Partido.java
@@ -2,7 +2,6 @@ package dobleyfalta.partidos_services.models;
 
 import java.sql.Date;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
