8aad379dec9f740ae75637772731a1a25ecb856e
emnemeGH <emaneme05@gmail.com>
2025-11-13 02:36:48 -0300

    Tablas

--- partidos-services/src/main/java/dobleyfalta/partidos_services/controllers/TablaController.java
+++ partidos-services/src/main/java/dobleyfalta/partidos_services/controllers/TablaController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import dobleyfalta.partidos_services.DTO.TablaDTO;
 import dobleyfalta.partidos_services.services.TablaService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
